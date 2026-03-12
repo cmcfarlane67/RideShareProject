4ff4b49db4bf3d1d87d468afaee8bcd1f91e8c1a
Catherine McFarlane <cmcfarlane26@students.polytechnic.org>
2026-03-11 20:17:43 -0700

    fixed i think

--- car.java
+++ car.java
@@ -1,15 +1,19 @@
 public class Car {
     
     private int location;
+
     public Car(int startLocation){
         location = startLocation;
     }
+
     public int getLocation(){
         return location;
     }
+
     public void moveRight(){
         location++;
     }
+
     public void moveLeft(){
         location--;
     }
